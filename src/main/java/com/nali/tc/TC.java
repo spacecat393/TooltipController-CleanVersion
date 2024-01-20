1ee9fb175c15ade66b0e81f9cf08636f2fe9c4e1
SpaceCat <jjcakecat@gmail.com>
2024-01-20 13:30:56 +0700

    init log

--- src/main/java/com/nali/tc/TC.java
+++ src/main/java/com/nali/tc/TC.java
@@ -2,9 +2,8 @@ package com.nali.tc;
 
 import com.nali.tc.system.Reference;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME)
@@ -13,11 +12,5 @@ public class TC
     @Instance
     public static TC I;
 
-    public static Logger LOGGER;
-
-    @EventHandler
-    public void onFMLPreInitializationEvent(FMLPreInitializationEvent event)
-    {
-        LOGGER = event.getModLog();
-    }
+    public static Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
 }
