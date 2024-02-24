65619f0e9dfff1b52139d68f773ff8c2ea90876e
SpaceCat <jjcakecat@gmail.com>
2024-02-24 18:09:03 +0700

    clean

--- src/main/java/com/nali/tc/TC.java
+++ src/main/java/com/nali/tc/TC.java
@@ -3,14 +3,10 @@ package com.nali.tc;
 import com.nali.tc.system.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME)
 public class TC
 {
     @Instance
     public static TC I;
-
-    public static Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
 }
